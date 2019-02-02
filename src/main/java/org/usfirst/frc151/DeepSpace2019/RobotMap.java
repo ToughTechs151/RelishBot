51789f94f9b99fc1e4dc46ab6e8f74a53dfd7fac
Emily Guo <lilycaroline1717@gmail.com>
2019-02-02 11:15:11 -0500

    Updated Chassis

---
+++ src/main/java/org/usfirst/frc151/DeepSpace2019/RobotMap.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc151.DeepSpace2019;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    public static final int FRONT_RIGHT = 2;
+    public static final int BACK_RIGHT = 3;
+    public static final int FRONT_LEFT = 0;
+    public static final int BACK_LEFT = 1;
+
+    public static final int RIGHT_JOYSTICK_VERTICAL_AXIS = 5;
+    public static final int LEFT_JOYSTICK_VERTICAL_AXIS = 1;
+}
