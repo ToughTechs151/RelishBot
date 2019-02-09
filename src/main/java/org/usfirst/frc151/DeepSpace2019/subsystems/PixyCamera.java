ec0cf1d41200b24f3c952a7ce85721848e8ddd2c
Emily Guo <lilycaroline1717@gmail.com>
2019-02-09 16:07:54 -0500

    Updated Pixy

--- src/main/java/org/usfirst/frc151/DeepSpace2019/subsystems/PixyCamera.java
+++ src/main/java/org/usfirst/frc151/DeepSpace2019/subsystems/PixyCamera.java
@@ -31,28 +31,14 @@ public class PixyCamera {
 		name = "Pixy_" + id;
 	}
 
-	// This method parses raw data from the pixy into readable integers
 	public int cvt(byte upper, byte lower) {
 		return (((int) upper & 0xff) << 8) | ((int) lower & 0xff);
 	}
 
-	// This method gathers data, then parses that data, and assigns the ints to
-	// global variables
-	// The
-	// signature
-	// should
-	// be
-	// which
-	// number
-	// object
-	// in
-	// pixymon you are trying to get data for
 	public PixyPacket readPacket(int Signature) throws PixyException {
 		int Checksum;
 		int Sig;
 		byte[] rawData = new byte[32];
-		// SmartDashboard.putString("rawData", rawData[0] + " " + rawData[1] + "
-		// " + rawData[15] + " " + rawData[31]);
 		try {
 			pixy.readOnly(rawData, 32);
 		} catch (RuntimeException e) {
@@ -65,18 +51,12 @@ public class PixyCamera {
 			return null;
 		}
 		for (int i = 0; i <= 16; i++) {
-			int syncWord = cvt(rawData[i + 1], rawData[i + 0]); // Parse first 2
-																// bytes
-			if (syncWord == 0xaa55) { // Check is first 2 bytes equal a "sync
-										// word", which indicates the start of a
-										// packet of valid data
-				syncWord = cvt(rawData[i + 3], rawData[i + 2]); // Parse the
-																// next 2 bytes
-				if (syncWord != 0xaa55) { // Shifts everything in the case that
-											// one syncword is sent
+			int syncWord = cvt(rawData[i + 1], rawData[i + 0]); 
+			if (syncWord == 0xaa55) {
+				syncWord = cvt(rawData[i + 3], rawData[i + 2]); 
+				if (syncWord != 0xaa55) { 
 					i -= 2;
 				}
-				// This next block parses the rest of the data
 				Checksum = cvt(rawData[i + 5], rawData[i + 4]);
 				Sig = cvt(rawData[i + 7], rawData[i + 6]);
 				if (Sig <= 0 || Sig > packets.length) {
@@ -88,8 +68,6 @@ public class PixyCamera {
 				packets[Sig - 1].Y = cvt(rawData[i + 11], rawData[i + 10]);
 				packets[Sig - 1].Width = cvt(rawData[i + 13], rawData[i + 12]);
 				packets[Sig - 1].Height = cvt(rawData[i + 15], rawData[i + 14]);
-				// Checks whether the data is valid using the checksum *This if
-				// block should never be entered*
 				if (Checksum != Sig + packets[Sig - 1].X + packets[Sig - 1].Y + packets[Sig - 1].Width
 						+ packets[Sig - 1].Height) {
 					packets[Sig - 1] = null;
@@ -99,8 +77,6 @@ public class PixyCamera {
 			} else
 				SmartDashboard.putNumber("syncword: ", syncWord);
 		}
-		// Assigns our packet to a temp packet, then deletes data so that we
-		// dont return old data
 		PixyPacket pkt = packets[Signature - 1];
 		packets[Signature - 1] = null;
 		return pkt;
@@ -131,11 +107,6 @@ public class PixyCamera {
 	}
 
 	private PixyPacket readBlock(int checksum) {
-		// See Object block format section in
-		// http://www.cmucam.org/projects/cmucam5/wiki/Porting_Guide#Object-block-format
-		// Each block is 14 bytes, but we already read 2 bytes for checksum in
-		// caller so now we need to read rest.
-
 		byte[] data = readData(12);
 		if (data == null) {
 			return null;
@@ -166,12 +137,6 @@ public class PixyCamera {
 	public boolean getStart() {
 		int numBytesRead = 0;
 		int lastWord = 0xffff;
-		// This while condition was originally true.. may not be a good idea if
-		// something goes wrong robot will be stuck in this loop forever. So
-		// let's read some number of bytes and give up so other stuff on robot
-		// can have a chance to run. What number of bytes to choose? Maybe size
-		// of a block * max number of signatures that can be detected? Or how
-		// about size of block and max number of blocks we are looking for?
 		while (numBytesRead < (OBJECT_SIZE * MAX_SIGNATURES)) {
 			int word = readWord();
 			numBytesRead += 2;
@@ -193,7 +158,6 @@ public class PixyCamera {
 	private boolean skipStart = false;
 
 	public PixyPacket[] readBlocks() {
-		// This has to match the max block setting in pixymon?
 		int maxBlocks = 2;
 		PixyPacket[] blocks = new PixyPacket[maxBlocks];
 
@@ -205,16 +169,12 @@ public class PixyCamera {
 			skipStart = false;
 		}
 		for (int i = 0; i < maxBlocks; i++) {
-			// Should we set to empty PixyPacket? To avoid having to check for
-			// null in callers?
 			blocks[i] = null;
 			int checksum = readWord();
 			if (checksum == START_WORD) {
-				// we've reached the beginning of the next frame
 				skipStart = true;
 				return blocks;
 			} else if (checksum == START_WORD_CC) {
-				// we've reached the beginning of the next frame
 				skipStart = true;
 				return blocks;
 			} else if (checksum == 0) {
